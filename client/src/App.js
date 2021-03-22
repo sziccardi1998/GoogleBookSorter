010fb50dcf6e5eac9926b6792142beb2751aa1bb
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-21 20:21:32 -0400

    page routing added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// will need to import components and pages here
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path={["/", "/home"]}>
+          <Home />
+        </Route>
+        <Route exact path={["/saved"]}>
+          <Saved />
+        </Route>
+        <Route exact path="/books/:id">
+          <Detail />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
